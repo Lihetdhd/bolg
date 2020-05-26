43b6d11fb14059597df98ad67ebc4fe80c6f8db3
Administratorss <940164665@qq.com>
2020-05-26 18:05:46 +0800

    添加头部导航和淘宝矢量

---
+++ src/views/home/components/menu_navigation.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="menu">
+    <div class="menu_box">
+      <img src="~@images/猫.png" />
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+      >
+        <el-menu-item index="1" v-for="menu in menuList" :key="menu">
+          <span slot="title">{{ menu.title }}</span>
+          <i class="iconfont" v-if="menu.icon" :class="menu.icon"></i>
+        </el-menu-item>
+      </el-menu>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuList: [
+        {
+          title: "首页",
+        },
+        {
+          title: "音乐",
+          icon: "icon-erji",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.menu_box {
+  width: 750px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+.menu_box img {
+  width: 60px;
+}
+</style>
+<style>
+.menu .el-menu {
+  border: none;
+}
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header>
+      <Menu> </Menu>
+    </header>
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import Menu from "./components/menu_navigation";
+export default {
+  components: {
+    Menu,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+header {
+  box-shadow: 1px 1px 25px 0 #f0f0f0;
+}
+</style>
